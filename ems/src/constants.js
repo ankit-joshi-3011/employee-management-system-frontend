9c8bc599fa97ac4ce3679d5af931f027af72048f
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 03:08:40 +0530

    Added a file called constants.js that stores the constants that we will be using throughout the React application

---
+++ ems/src/constants.js
@@ -0,0 +1,4 @@
+// We should not hard code this URL. Currently doing this just
+// for development purposes.
+export const API_BACKEND_URL = 'http://localhost:8080'
+export const EMPLOYEE_REST_API_BASE_URI_PATH = '/api/employees';
