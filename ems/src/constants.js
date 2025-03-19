86332416809c46a979efe5a5c6f7993b4bdb80f8
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 12:15:48 +0530

    Added a couple of constants REST_API_BASE_URI_PATH and DEPARTMENT_REST_API_BASE_URI_PATH that hold the base URI paths to the REST APIs as a whole and to the REST APIs that deal with the department entity specifically respectively

--- ems/src/constants.js
+++ ems/src/constants.js
@@ -1,4 +1,6 @@
 // We should not hard code this URL. Currently doing this just
 // for development purposes.
 export const API_BACKEND_URL = 'http://localhost:8080'
-export const EMPLOYEE_REST_API_BASE_URI_PATH = '/api/employees';
+export const REST_API_BASE_URI_PATH = '/api';
+export const EMPLOYEE_REST_API_BASE_URI_PATH = REST_API_BASE_URI_PATH + '/employees';
+export const DEPARTMENT_REST_API_BASE_URI_PATH = REST_API_BASE_URI_PATH + '/departments';
