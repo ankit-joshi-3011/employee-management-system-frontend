5cc4ecc883d470193983507f6caa533f2347fd9e
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-19 13:51:37 +0530

    Added the getActiveManagersByDepartment() to get the active managers by department

--- ems/src/services/employeeService.js
+++ ems/src/services/employeeService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_BACKEND_URL, DEPARTMENT_REST_API_BASE_URI_PATH, EMPLOYEE_REST_API_BASE_URI_PATH } from "../constants";
+import { API_BACKEND_URL, DEPARTMENT_REST_API_BASE_URI_PATH, EMPLOYEE_REST_API_BASE_URI_PATH, RETRIEVE_ACTIVE_MANAGERS_BY_DEPARTMENT_API_URI_PATH } from "../constants";
 
 export function getAllDepartments() {
     return axios.get(API_BACKEND_URL + DEPARTMENT_REST_API_BASE_URI_PATH);
@@ -7,4 +7,10 @@ export function getAllDepartments() {
 
 export function createEmployee(newEmployeeDetails) {
     return axios.post(API_BACKEND_URL + EMPLOYEE_REST_API_BASE_URI_PATH, newEmployeeDetails);
+}
+
+export function getActiveManagersByDepartment(departmentId) {
+    return axios.get(API_BACKEND_URL + RETRIEVE_ACTIVE_MANAGERS_BY_DEPARTMENT_API_URI_PATH, {
+        params: { departmentId }
+    });
 }
